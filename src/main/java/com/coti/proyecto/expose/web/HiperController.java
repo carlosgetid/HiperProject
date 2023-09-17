d701bdf0adb862fb9851197416a855d3dcc3bd59
playcar00 <carlospgp13@gmail.com>
2023-09-17 00:30:49 -0500

    feat: get services

--- src/main/java/com/coti/proyecto/expose/web/HiperController.java
+++ src/main/java/com/coti/proyecto/expose/web/HiperController.java
@@ -1,6 +1,9 @@
 package com.coti.proyecto.expose.web;
 
+import com.coti.proyecto.hiper.model.business.Servicio;
+import java.util.List;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,4 +17,18 @@ public class HiperController {
     return id;
   }
 
+  @GetMapping("/servicios")
+  public ResponseEntity<List<Servicio>> obtenerServicios() {
+    Servicio e1 = new Servicio();
+    e1.setNombre("Soporte Tecnico Estandar");
+    e1.setCodigo("01");
+    e1.setPrecio("3000");
+
+    Servicio e2 = new Servicio();
+    e2.setNombre("Soporte Premium 24/7");
+    e2.setCodigo("02");
+    e2.setPrecio("3500");
+
+    return ResponseEntity.ok(List.of(e1, e2));
+  }
 }
