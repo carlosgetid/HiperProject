dde47781a6e3ab120d879fab05d7d79671f3b493
playcar00 <carlospgp13@gmail.com>
2023-09-22 12:57:30 -0500

    feat: save and list outsourcing services

--- src/main/java/com/coti/proyecto/expose/web/OutsourcingController.java
+++ src/main/java/com/coti/proyecto/expose/web/OutsourcingController.java
@@ -1,18 +1,25 @@
 package com.coti.proyecto.expose.web;
 
+import com.coti.proyecto.hiper.model.business.outsourcing.CotizacionOutsourcing;
 import com.coti.proyecto.hiper.model.business.outsourcing.Servicio;
 import com.coti.proyecto.hiper.model.business.outsourcing.SubServicio;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @ComponentScan(value = "com.proyecto", lazyInit = true)
 public class OutsourcingController {
+
+  private List<CotizacionOutsourcing> cotizacionOutsourcings = new ArrayList<>();
 
   @GetMapping("/outsourcing/servicios/subservicios")
   public ResponseEntity<List<Servicio>> obtenerServicios() {
@@ -199,5 +206,32 @@ public class OutsourcingController {
     a23.setSubServicios(List.of(b23, b24, b25, b26));
 
     return ResponseEntity.ok(List.of(a2, a6, a9, a12, a15, a18, a21, a23));
+  }
+
+
+  @PostMapping("/outsourcing/grabar")
+  public String grabar(@RequestBody CotizacionOutsourcing c1) {
+    cotizacionOutsourcings.stream()
+        .filter(x -> Objects.equals(x.getNroCotizacion(), c1.getNroCotizacion()))
+        .findFirst()
+        .ifPresentOrElse(x -> {
+          x.setServicio(c1.getServicio());
+          x.setSubServicio(c1.getSubServicio());
+          x.setHoras(c1.getHoras());
+          x.setPersonal(c1.getPersonal());
+          x.setTipo(c1.getTipo());
+          x.setCostoXServicio(c1.getCostoXServicio());
+        }, () -> cotizacionOutsourcings.add(c1));
+
+    System.out.println("========================================");
+    cotizacionOutsourcings.forEach(x-> System.out.println(x.toString()));
+    System.out.println("========================================");
+
+    return "Guardado exitosamente";
+  }
+
+  @GetMapping("/outsourcing/listar")
+  public ResponseEntity<List<CotizacionOutsourcing>> listar() {
+    return ResponseEntity.ok(cotizacionOutsourcings);
   }
 }
